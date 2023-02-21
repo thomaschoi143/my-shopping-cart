2bb2c3bb7396c69aa1acfc8562e091a0620c3626
thomaschoi143 <thomasa143a@gmail.com>
2023-02-22 01:11:55 +1100

    not work

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,33 @@ import Root from "./components/Root";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from "react-router-dom";
 import HomePage from "./pages/home";
 import ItemDetailsPage from "./pages/itemDetails";
 import NotFoundPage from "./pages/notFound";
 
 if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
 }
 
+const { worker } = require("./mocks/browser");
+worker.start();
+
 const appRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<HomePage />} />
-      <Route path="items/:name" element={<ItemDetailsPage />} />
-      <Route path="not-found" element={<NotFoundPage />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route path="/" element={<Root />}>
+			<Route index element={<HomePage />} />
+			<Route path="items/:name" element={<ItemDetailsPage />} />
+			<Route path="not-found" element={<NotFoundPage />} />
+		</Route>
+	)
 );
 
 function App() {
-  return <RouterProvider router={appRouter} />;
+	return <RouterProvider router={appRouter} />;
 }
 
 export default App;
